742bfb5672b3bf3e95fda76600da0c28e13b458d
Frost-Phoenix <67cyril6767@gmail.com>
2024-09-12 22:19:08 +0200

    Init

---
+++ modules/home/default.nix
@@ -0,0 +1,32 @@
+{inputs, username, host, ...}: {
+  imports = [
+    ./aseprite/aseprite.nix           # pixel art editor
+    ./audacious.nix                   # music player
+    ./bat.nix                         # better cat command
+    ./btop.nix                        # resouces monitor 
+    ./cava.nix                        # audio visualizer
+    ./discord/discord.nix                     # discord with catppuccin theme
+    ./fastfetch.nix                         # fetch tool
+    ./fzf.nix                         # fuzzy finder
+    ./gaming.nix                      # packages related to gaming
+    ./git.nix                         # version control
+    ./gnome.nix                         # gnome apps
+    ./gtk.nix                         # gtk theme
+    ./hyprland                        # window manager
+    ./kitty.nix                       # terminal
+    ./swaync/swaync.nix               # notification deamon
+    ./micro.nix                       # nano replacement
+    ./nvim.nix                        # neovim editor
+    ./packages.nix                    # other packages
+    ./retroarch.nix  
+    ./rofi.nix                        # launcher
+    ./scripts/scripts.nix             # personal scripts
+    ./spicetify.nix                   # spotify client
+    ./starship.nix                    # shell prompt
+    ./swaylock.nix                    # lock screen
+    ./vscodium.nix                    # vscode forck
+    ./waybar                          # status bar
+    ./xdg-mimes.nix                   # xdg config
+    ./zsh.nix                         # shell
+  ];
+}
